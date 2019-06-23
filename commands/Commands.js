18a2914cddda5a51f5fc4ae1df5b16fd2bc0950c
Re3ker <registhis@gmail.com>
2019-06-23 13:49:03 +0200

    Added CommandsHandler

---
+++ commands/Commands.js
@@ -0,0 +1,4 @@
+module.exports.helpCommand = require('./helpCommand').run;
+module.exports.interactionCommand = require('./interactionCommand').run;
+module.exports.gifCommand = require('./gifCommand').run;
+module.exports.activitiesCommand = require('./activitiesCommand').run;
