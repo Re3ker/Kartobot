7cfb5d846bc8162ca91024ad7a5e1a9df4c79bc3
Re3ker <registhis@gmail.com>
2019-06-23 13:49:44 +0200

    Added gif command

---
+++ commands/gifCommand.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+
+module.exports.run = function(gClient,message, args) {
+  let search_text = args.join(' ');
+  gClient.search('gifs', {'q': search_text, 'sort': 'relevant'})
+  .then((response) => {
+    if(response.pagination.count == 0) return message.channel.send(':no_entry: `Are you sure about that?`');
+    let {data} = response;
+    let gifObj = data[Math.floor(Math.random()*data.length)];
+
+    let rich = new Discord.RichEmbed();
+    rich.setColor(0x793fd1);
+    rich.setImage(gifObj.images.original.url);
+    message.channel.send(rich);
+  })
+  .catch((err) => {
+    console.log(err);
+    
+  })
+};
