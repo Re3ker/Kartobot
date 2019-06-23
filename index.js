876addffbe2bd4ea9a6377bfabba91a2d2459be5
Re3ker <registhis@gmail.com>
2019-06-23 13:55:40 +0200

    removed config for deployment

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const config = require('./config');
+// const config = require('./config');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const GphApiClient = require('giphy-js-sdk-core');
