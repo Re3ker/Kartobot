c68b24a620d911ee0cc281811e2b602594fa4447
Re3ker <registhis@gmail.com>
2019-06-26 19:50:59 +0200

    Added new commands

--- commands/Commands.js
+++ commands/Commands.js
@@ -3,6 +3,10 @@ module.exports.helpCommand = require('./main/helpCommand');
 module.exports.interactionCommand = require('./main/interactionCommand');
 module.exports.gifCommand = require('./main/gifCommand');
 module.exports.activitiesCommand = require('./main/activitiesCommand');
+module.exports.jokeCommand = require('./main/jokeCommand');
+
+// nsfw commands
+module.exports.konaCommand = require('./nsfw/konaCommand');
 
 // master commands
 module.exports.leaveCommand = require('./master/leaveCommand');
