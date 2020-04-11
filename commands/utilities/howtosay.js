e44c676f3819ea63e8abc3f3f1a913f72f9405c2
KartoDev <1680301-KartoDev@users.noreply.gitlab.com>
2020-04-11 14:18:10 +0200

    moved howtoday command

--- commands/utilities/howtosay.js
+++ commands/utilities/howtosay.js
@@ -3,16 +3,16 @@ import https from 'https';
 export default {
   name: 'howtosay',
   description: 'how to say a given word',
-  category: 'general',
+  category: 'utilities',
   alias: ['hts'],
   args: ['word'],
   async execute(message, args) {
-    let processMsg = await message.channel.send(":orange_circle: Loading...").then(msg => msg);
+    let processMsg = await message.channel.send(":orange_circle: Processing...").then(msg => msg);
     https.get(`https://howjsay.com/mp3/${args[0]}.mp3`, function(response) {
       if (response.headers['content-type'] !== 'audio/mpeg') {
         processMsg.edit(':red_circle: I cannot say that, I\'m sorry.');
       } else {
-        processMsg.delete();
+        processMsg.delete().catch(error => console.error);
         message.channel.send(new Discord.MessageAttachment(response, `${args[0]}.mp3`));
       }
     });
