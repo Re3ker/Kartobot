631ee71b2606a18ac9f92e5e63552f5754cfa513
KartoDev <1680301-KartoDev@users.noreply.gitlab.com>
2020-04-11 14:16:23 +0200

    updated config example

--- config.example.js
+++ config.example.js
@@ -1,4 +1,6 @@
 export default {
-  DISCORD_TOKEN: "",
+  DISCORD_TOKEN: "TOKEN_GOES_HERE",
+  PREFIX: ';',
   BOT_COLOR: "#fbb019",
+  DB_CONNECTION: 'mysql://USERNAME:PASSWORD@localhost:3306/DATABASE_NAME'
 }
